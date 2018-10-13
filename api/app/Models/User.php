12e9345dbbb4b106462055ab5776294724b071fd
Manan <jn.manan25@gmail.com>
2018-10-13 13:55:00 +0530

    Basic frontend for login

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements \Illuminate\Contracts\Auth\Authenticatable
 {
     protected $table = 'users';
     protected $fillable = ['username', 'fullname', 'email', 'role'];
