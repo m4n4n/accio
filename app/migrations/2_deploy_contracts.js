fb30786d528b0b66cb99702fa2684fbf513f4617
Vedansh Bhartia <vedanshbhartia@gmail.com>
2018-10-14 03:35:45 +0530

    Fixes migration and app.js

--- app/migrations/2_deploy_contracts.js
+++ app/migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var escrow = artifacts.require("./escrow.sol");
+var create = artifacts.require("./create.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var transfer = artifacts.require("./transfer.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+    deployer.deploy(Ownable);
+    deployer.link(Ownable, create);
+    deployer.deploy(create);
+    deployer.link(create, transfer);
+    deployer.deploy(transfer);
+    deployer.link(create, escrow);
+    deployer.deploy(escrow);
+}
--- app/server.js
+++ app/server.js
@@ -11,48 +11,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use('/', express.static('public_static'));
+// app.get('/getAccounts', (req, res) => {
+//   console.log("**** GET /getAccounts ****");
+//   truffle_connect.start(function (answer) {
+//     res.send(answer);
+//   })
+// });
 
-app.get('/getAccounts', (req, res) => {
-  console.log("**** GET /getAccounts ****");
-  truffle_connect.start(function (answer) {
+app.post('/newTransaction', (req, res) => {
+  console.log("**** New Transaction ****");
+  console.log(req.body);
+  let  sender = req.body.sender, 
+         courier = req.body.courier, 
+         transaction_id = req.body.transaction_id, 
+         courier_eth = req.body.courier_eth, 
+         sender_eth = req.body.sender_eth; 
+
+  truffle_connect.initTransaction(sender, courier, transaction_id, courier_eth, sender_eth, (answer) => {
     res.send(answer);
-  })
-});
-
-app.post('/getBalance', (req, res) => {
-  console.log("**** GET /getBalance ****");
-  console.log(req.body);
-  let currentAcount = req.body.account;
-
-  truffle_connect.refreshBalance(currentAcount, (answer) => {
-    let account_balance = answer;
-    truffle_connect.start(function(answer){
-      // get list of all accounts and send it along with the response
-      let all_accounts = answer;
-      response = [account_balance, all_accounts]
-      res.send(response);
-    });
-  });
-});
-
-app.post('/sendCoin', (req, res) => {
-  console.log("**** GET /sendCoin ****");
-  console.log(req.body);
-
-  let amount = req.body.amount;
-  let sender = req.body.sender;
-  let receiver = req.body.receiver;
-
-  truffle_connect.sendCoin(amount, sender, receiver, (balance) => {
-    res.send(balance);
   });
 });
 
 app.listen(port, () => {
 
   // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-  truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
   console.log("Express Listening at http://localhost:" + port);
 
