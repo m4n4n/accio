1db6cc98cf91d104cf7bbb4eb8bfbcaa889ca507
Kartikey Pandey <pandeykartikey99@gmail.com>
2018-10-13 19:57:16 +0530

    Fixes issue with navbar

--- ui/public/scripts/init.js
+++ ui/public/scripts/init.js
@@ -26,15 +26,22 @@ $(document).ready(function(){
 function userReload() {
 		console.log("'*************");
 		console.log(user);
-		// if(Object.keys(user).length !== 0) {
-			console.log(document.querySelector('#loginnav'));
-			login = document.querySelector('#loginnav');
-			login.classList.add('hide');
-			// document.getElementById('usernav').innerHTML = user.name;
-			console.log(document.getElementById('loginnav').style.display);
-			console.log(document.getElementById('loginnav'));
-		// } else {
-			// document.getElementById('signupnav').setAttribute('style', 'display: block;');
-			// document.getElementById('loginnav').setAttribute('style', 'display: block;');
-			// document.getElementById('usernav').setAttribute('style', 'display: none;');
+		if(Object.keys(user).length !== 0) {
+			document.getElementById('signupnavmob').style.display =  'none';
+			document.getElementById('signupnav').style.display =  'none';
+			document.getElementById('loginnavmob').style.display =  'none';
+			document.getElementById('loginnav').style.display =  'none';
+			document.getElementById('usernav').innerHTML = user.name;
+			document.getElementById('usernavmob').innerHTML = user.name;
+			document.getElementById('usernavmob').style.display =  'block';
+			document.getElementById('usernav').style.display =  'block';
+		} else {
+			document.getElementById('signupnavmob').style.display =  'block';
+			document.getElementById('signupnav').style.display =  'block';
+			document.getElementById('loginnavmob').style.display =  'block';
+			document.getElementById('loginnav').style.display =  'block';
+
+			document.getElementById('usernavmob').style.display =  'none';
+			document.getElementById('usernav').style.display =  'none';
 		}
+	}
